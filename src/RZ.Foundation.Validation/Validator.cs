0b3d42e4b8e6c36c4a0284c9db1c65c4bfe4a930
Ruxo Zheng <rz@just.sent.as>
2025-01-27 13:53:53 +0700

    Add transformer

--- src/RZ.Foundation.Validation/Validator.cs
+++ src/RZ.Foundation.Validation/Validator.cs
@@ -4,10 +4,13 @@ namespace RZ.Foundation.Validation;
 
 public static class Validator
 {
-    public static Func<T, string?> OfSingleError<T>(Action<IRuleBuilderInitial<SingleValue<T>,T>> builder) {
-        var validator = new SingleValueValidator<T>(builder);
+    public static Func<T, string?> OfSingleError<T>(Action<IRuleBuilderInitial<SingleValue<T>,T>> builder)
+        => OfSingleError<T,T>(builder, x => x);
+
+    public static Func<A, string?> OfSingleError<A,B>(Action<IRuleBuilderInitial<SingleValue<B>,B>> builder, Func<A,B> mapper) {
+        var validator = new SingleValueValidator<B>(builder);
         return x => {
-            var errors = validator.Validate(new SingleValue<T>(x));
+            var errors = validator.Validate(new SingleValue<B>(mapper(x)));
             return errors.IsValid ? null : errors.Errors.First().ErrorMessage;
         };
     }
@@ -16,6 +19,14 @@ public static class Validator
         var validator = new SingleValueValidator<T>(builder);
         return x => {
             var errors = validator.Validate(new SingleValue<T>(x));
+            return errors.IsValid ? [] : errors.Errors.Select(e => e.ErrorMessage);
+        };
+    }
+
+    public static Func<A, IEnumerable<string>> Of<A,B>(Action<IRuleBuilderInitial<SingleValue<B>,B>> builder, Func<A,B> mapper) {
+        var validator = new SingleValueValidator<B>(builder);
+        return x => {
+            var errors = validator.Validate(new SingleValue<B>(mapper(x)));
             return errors.IsValid ? [] : errors.Errors.Select(e => e.ErrorMessage);
         };
     }
