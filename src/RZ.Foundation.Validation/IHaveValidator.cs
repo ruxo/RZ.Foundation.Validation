acff2f1afa5f0c2133fc888f55f5543ab64a32cc
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-01-20 18:37:18 +0700

    Upgrade Foundation library. Change paradigm to Outcome

--- src/RZ.Foundation.Validation/IHaveValidator.cs
+++ src/RZ.Foundation.Validation/IHaveValidator.cs
@@ -9,20 +9,14 @@ public interface IHaveValidator<in T>
     static abstract IValidator<T> Validator { get; }
 }
 
-[PublicAPI]
 public static class IHaveValidatorExtensions
 {
-    public static T Validate<T>(this T updated) where T: IHaveValidator<T> {
-        var invalid = T.Validator.Validate(updated).Errors.FirstOrDefault();
-        if (invalid is not null)
-            throw new ErrorInfoException(StandardErrorCodes.ValidationFailed, invalid.ErrorMessage);
-        return updated;
-    }
-
-    public static Outcome<T> TryValidate<T>(this T updated) where T: IHaveValidator<T> {
-        var invalid = T.Validator.Validate(updated).Errors.FirstOrDefault();
-        if (invalid is not null)
-            return new ErrorInfo(StandardErrorCodes.ValidationFailed, invalid.ErrorMessage);
-        return updated;
+    extension<T>(T updated) where T : IHaveValidator<T>
+    {
+        [PublicAPI]
+        public Outcome<T> Validate()
+            => T.Validator.Validate(updated).Errors.FirstOrDefault() is { } invalid
+                   ? new ErrorInfo(StandardErrorCodes.ValidationFailed, invalid.ErrorMessage)
+                   : updated;
     }
 }
